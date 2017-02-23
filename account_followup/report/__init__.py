06ae6a126afa24359fcad3d6418ea5ca61026a9a
cubells <vicent@vcubells.net>
2017-02-23 22:20:47 +0100

    [MIG][9.0] account_followup module

---
+++ account_followup/report/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2004-2010 Tiny SPRL (<http://tiny.be>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import account_followup_print
+import account_followup_report
